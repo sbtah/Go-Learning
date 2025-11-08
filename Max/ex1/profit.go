5438f117f04365e6e3d2b47746eb097d0c7dbeb1
Grzegorz.Z <grzegorz.zy@protonmail.com>
2025-11-08 15:54:51 +0100

    Save progress.

--- Max/ex1/profit.go
+++ Max/ex1/profit.go
@@ -4,14 +4,33 @@ import "fmt"
 
 func main() {
 	var monthlyRevenue float64
-	fmt.Print("What is your monthly revenue?: ")
+	fmt.Print("What is your monthly revenue value?: ")
 	fmt.Scan(&monthlyRevenue)
-
 	fmt.Println(monthlyRevenue)
+
+	var monthlyExpenses float64
+	fmt.Print("What are your monthly expenses value?: ")
+	fmt.Scan(&monthlyExpenses)
+	fmt.Println(monthlyExpenses)
 
 	var taxRate float64
 	fmt.Print("What is tax rate?: ")
 	fmt.Scan(&taxRate)
-
 	fmt.Println(taxRate)
+
+	// Calculate EBT
+	var ebt float64 = monthlyRevenue - monthlyExpenses
+
+	// Calculate Tax:
+	var tax float64 = ebt * taxRate
+
+	// Calculate net income (after tax, profit)
+	var netIncome float64 = ebt - tax
+
+	// Calculate ratio (ebt / profit)
+	var ratio float64 = ebt / netIncome
+
+	fmt.Print("Your EBT is: ")
+	fmt.Println(ebt)
+
 }
